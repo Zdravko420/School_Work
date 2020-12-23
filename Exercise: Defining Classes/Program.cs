544ff2a3231ca3ad974d90d46a9b22fdcdb99a6f
Zdravko420 <43958501+Zdravko420@users.noreply.github.com>
2020-12-23 17:52:01 +0200

    Add files via upload

---
+++ Exercise: Defining Classes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+  public  class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+    }
+}
+    
